3ac95415f4b1e34bba12aa66d2a91ebd731de1a4
Pelot22 <pf.noblet@gmail.com>
2024-09-24 19:41:24 +0200

    Evolution debut collision

--- main.lua
+++ main.lua
@@ -10,6 +10,18 @@ function math.dist(x1,y1, x2,y2) return ((x2-x1)^2+(y2-y1)^2)^0.5 end
 
 function math.angle(x1,y1, x2,y2) return math.atan2(y2-y1, x2-x1) end
 
+function collide(a1, a2)
+    if (a1==a2) then return false end
+    local dx = a1.x - a2.x
+    local dy = a1.y - a2.y
+    if (math.abs(dx) < a1.image:getWidth()+a2.image:getWidth()) then
+     if (math.abs(dy) < a1.image:getHeight()+a2.image:getHeight()) then
+      return true
+     end
+    end
+    return false
+end
+
 screenWidth = 0
 screenHeight = 0
 
